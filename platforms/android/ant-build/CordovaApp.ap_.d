fe57e930e11293e94e2edc94c33bc2efe2458140
Hari hara prasad Viswanathan <hr.hr.prasath@gmail.com>
2015-04-24 18:14:04 +0530

    revert back to ionic framework
    revert back to ionic framework

--- platforms/android/ant-build/CordovaApp.ap_.d
+++ platforms/android/ant-build/CordovaApp.ap_.d
@@ -61,8 +61,8 @@ C:\Users\Hari\Documents\GitHub\DVH\platforms\android\assets\www\icons\test.png \
 C:\Users\Hari\Documents\GitHub\DVH\platforms\android\assets\www\icons\thold.png \
 C:\Users\Hari\Documents\GitHub\DVH\platforms\android\assets\www\icons\wl.png \
 C:\Users\Hari\Documents\GitHub\DVH\platforms\android\assets\www\img\logo.png \
-C:\Users\Hari\Documents\GitHub\DVH\platforms\android\assets\www\js\angular.min.js \
 C:\Users\Hari\Documents\GitHub\DVH\platforms\android\assets\www\js\Explorer.js \
+C:\Users\Hari\Documents\GitHub\DVH\platforms\android\assets\www\js\ionic.bundle.min.js \
 C:\Users\Hari\Documents\GitHub\DVH\platforms\android\assets\www\js\NgDicomViewer.js \
 C:\Users\Hari\Documents\GitHub\DVH\platforms\android\assets\www\js\openjpeg.js \
 C:\Users\Hari\Documents\GitHub\DVH\platforms\android\assets\www\plugins\org.apache.cordova.file \
